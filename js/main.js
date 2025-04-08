c93b70385069d8bf17506c056ae22121f817b441
himanshu_rivuletdigital <himanshu@rivuletdigital.com>
2025-04-08 08:42:55 +0530

    added responsive menu functionlity

--- js/main.js
+++ js/main.js
@@ -16,5 +16,27 @@ window.addEventListener('resize', () => {
 
 // Responsive Navigation Menu 
 document.querySelector('.menu-toggle').addEventListener('click', function () {
-    document.querySelector('body').classList.toggle('active');
-  });
+    document.querySelector('body').classList.toggle('navigation-open');
+});
+
+document.querySelectorAll('li').forEach(li => {
+    if(li.querySelector('ul')) {
+        let = span = document.createElement('span');
+        span.innerHTML = '<i class="fa-solid fa-angle-down"></i>';
+        li.classList.add('has-submenu');
+        li.insertBefore(span, li.firstChild);
+    }
+});
+
+let submenuToggle = document.querySelectorAll(".has-submenu > span");
+submenuToggle.forEach((menuItem)=>{
+    menuItem.addEventListener("click", function(e){
+        const submenu = this.closest(".has-submenu");
+        if (submenu.classList.contains('openSubmenu')) {
+            this.closest(".has-submenu").classList.remove('openSubmenu');
+          } else {
+            submenu.style.display = "block";
+            this.closest(".has-submenu").classList.add('openSubmenu');
+          }
+    });
+});
